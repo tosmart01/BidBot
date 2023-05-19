645ce84b3525accc3de954a139fb915df1b16965
wangzhuo <wangzhuo@datagrand.com>
2023-05-20 00:13:45 +0800

    Update base.py

--- settings/base.py
+++ settings/base.py
@@ -125,7 +125,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/s/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = 'media'
